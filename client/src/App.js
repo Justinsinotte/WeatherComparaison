99e073d3d841370c08aa38ed33a0362e170fb995
Justin <justinsinotte@gmail.com>
2023-04-18 14:18:01 -0400

    API tests

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+
 function App() {
-  return <main>Hello World!</main>;
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
 }
 
 export default App;
