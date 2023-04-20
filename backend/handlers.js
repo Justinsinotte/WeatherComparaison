63057d913cd270ebcd454f4e13a943c6860c0286
Justin <justinsinotte@gmail.com>
2023-04-19 20:02:25 -0400

    Took off API codes.... my mistake.

--- backend/handlers.js
+++ backend/handlers.js
@@ -1,17 +1,3 @@
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
-// http://api.openweathermap.org/geo/1.0/direct?q=montreal,canada&limit=5&appid=af8a520e181ab2f19f9ba63d21531604
-
-// "lat": 45.5031824,
-// "lon": -73.5698065,
-// "country": "CA",
-// "state": "Quebec"
-
-// https://api.openweathermap.org/data/3.0/onecall?lat=45.5031824&lon=-73.5698065&appid=af8a520e181ab2f19f9ba63d21531604
-
-//api.openweathermap.org/data/2.5/weather?lat=45.5031824&lon=-73.5698065&appid=af8a520e181ab2f19f9ba63d21531604
-
-// https://api.openweathermap.org/data/2.5/weather?lat=45.5031824&lon=-73.5698065&appid=${API}&units=metric
 "use strict";
 const { MongoClient } = require("mongodb");
 
