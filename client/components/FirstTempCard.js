e8509e07918d577f31ce91c7316b6c34e1941b97
Justin <justinsinotte@gmail.com>
2023-04-26 20:25:29 -0400

    Messed around with some CSS.

--- client/components/FirstTempCard.js
+++ client/components/FirstTempCard.js
@@ -1,34 +1,12 @@
 import { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 import { Card, Icon } from "@rneui/themed";
 
 const FirstTempCard = () => {
   const [firstData, setFirstData] = useState();
 
-  const WeatherForecast = ({ firstData }) => {
-    const noonTemps = firstData.list.filter((data) =>
-      data.dt_txt.includes("12:00:00")
-    );
-    const renderForecastCards = () => {
-      return noonTemps.map((data) => {
-        const date = new Date(data.dt * 1000);
-        const dateOptions = {
-          month: "long",
-          day: "numeric",
-        };
-
-        const dayOfMonth = date.toLocaleDateString("en-US", dateOptions);
-        const dayOfWeek = date.toLocaleDateString("en-US", dateOptions);
-        return (
-          <View key={data.dt} style={styles.card}>
-            <Text style={styles.day}>{dayOfMonth}</Text>
-            <Text style={styles.temp}>{Math.ceil(data.main.temp)}°C</Text>
-          </View>
-        );
-      });
-    };
-    return renderForecastCards();
-  };
+  // const weatherIcon = `http://openweathermap.org/img/w/${firstData.weather[0].icon}.png`;
+  // console.log(firstData);
 
   useEffect(() => {
     fetch("http://10.0.0.102:3001/api/test")
@@ -47,14 +25,49 @@ const FirstTempCard = () => {
         console.log("Error fetching data:");
       });
   }, []);
-  console.log(firstData);
+  // console.log(firstData);
+
+  const WeatherForecast = ({ firstData }) => {
+    const noonTemps = firstData.list.filter((data) =>
+      data.dt_txt.includes("12:00:00")
+    );
+    const renderForecastCards = () => {
+      return noonTemps.map((data) => {
+        const date = new Date(data.dt * 1000);
+        const dateOptions = {
+          month: "long",
+          day: "numeric",
+        };
+        // console.log(data.weather[0].icon);
+        const dayOfMonth = date.toLocaleDateString("en-US", dateOptions);
+        const dayOfWeek = date.toLocaleDateString("en-US", dateOptions);
+        return (
+          <View key={data.dt} style={styles.card}>
+            <View style={styles.TempContainer}>
+              <Text style={styles.temp}>{Math.ceil(data.main.temp)}°C</Text>
+              <Text style={styles.day}>{dayOfMonth}</Text>
+              <Image
+                style={styles.icon}
+                source={{
+                  uri: `https://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+                }}
+              />
+            </View>
+          </View>
+        );
+      });
+    };
+    return renderForecastCards();
+  };
 
   return (
     <View style={styles.container}>
       {firstData !== undefined && (
         <View>
           <Text style={styles.title}>{`${firstData.city.name}'s weather`}</Text>
-          <WeatherForecast firstData={firstData} />
+          <View style={styles.InfoContainer}>
+            <WeatherForecast firstData={firstData} />
+          </View>
           {/* 
           <Text style={styles.description}>{`Current Temperature: ${Math.ceil(
             data.main.temp
@@ -76,8 +89,17 @@ const FirstTempCard = () => {
 };
 
 const styles = StyleSheet.create({
+  InfoContainer: {
+    display: "flex",
+    flexDirection: "row",
+    // backgroundColor: "pink",
+  },
   container: {
-    backgroundColor: "#fff",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+
+    // backgroundColor: "brown",
     borderRadius: 5,
     padding: 20,
     margin: 10,
@@ -98,6 +120,36 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 16,
   },
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    padding: 10,
+    // backgroundColor: "blue",
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  TempContainer: {
+    // backgroundColor: "red",
+  },
+  // day: {
+  //   display: "flex",
+  //   flexDirection: "row",
+  //   justifyContent: "space-between",
+  //   padding: 10,
+  //   backgroundColor: "#fff",
+  //   borderRadius: 5,
+  //   marginBottom: 10,
+  // },
+  // temp: {
+  //   display: "flex",
+  //   flexDirection: "row",
+  //   justifyContent: "space-between",
+  //   padding: 10,
+  //   backgroundColor: "#fff",
+  //   borderRadius: 5,
+  //   marginBottom: 10,
+  // },
 });
 
 export default FirstTempCard;
