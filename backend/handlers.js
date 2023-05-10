021bc1f62ccdd3b87b6dfe7ccf56421fd5791ac9
Justin <justinsinotte@gmail.com>
2023-05-09 20:14:43 -0400

    Finally hooked up AccuWeather, everythings builtin

--- backend/handlers.js
+++ backend/handlers.js
@@ -3,7 +3,7 @@ const { MongoClient } = require("mongodb");
 
 require("dotenv").config();
 const { MONGO_URI } = process.env;
-const { API, GEOAPI } = process.env;
+const { API, GEOAPI, ACCUAPI } = process.env;
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -31,25 +31,43 @@ const test = async (req, res) => {
 };
 
 const citiesGet = async (req, res) => {
-  const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": GEOAPI,
-      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-    },
-  };
-
   try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-    res.send(result); // send the result back to the client
+    // const response = await fetch(
+    //   `https://api.openweathermap.org/data/2.5/weather?lat=45.5031824&lon=-73.5698065&appid=${API}&units=metric`
+    // );
+
+    const response = await fetch(
+      `http://dataservice.accuweather.com/forecasts/v1/daily/1day/56186?apikey=${ACCUAPI}&metric=true`
+    );
+
+    const data = await response.json();
+    res.json(data);
   } catch (error) {
     console.error(error);
-    res.status(500).send("Internal Server Error"); // send an error response
+    res.status(500).json({ message: "Server error" });
   }
 };
+
+// const citiesGet = async (req, res) => {
+//   const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       "X-RapidAPI-Key": GEOAPI,
+//       "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+//     },
+//   };
+
+//   try {
+//     const response = await fetch(url, options);
+//     const result = await response.text();
+//     console.log(result);
+//     res.send(result); // send the result back to the client
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error"); // send an error response
+//   }
+// };
 
 module.exports = {
   test,
