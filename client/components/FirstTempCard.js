7ff02b63b5ea3a8969e9183fd73a45b35320985c
Justin <justinsinotte@gmail.com>
2023-06-05 16:55:26 -0400

    First Temp Card works now!!

--- client/components/FirstTempCard.js
+++ client/components/FirstTempCard.js
@@ -5,7 +5,7 @@ import Config from "react-native-config";
 
 const FirstTempCard = ({ firstData, firstOnSelectCity }) => {
   const { API } = process.env;
-  const [weatherIcon, setWeatherIcon] = useState(null);
+  const [firstWeatherFetch, setFirstWeatherFetch] = useState(null);
   console.log(`FirstTempCard API is ${API}`);
   console.log(`FirstTempCard firstOnCitySelect is ${firstOnSelectCity}`);
   console.log(`FirstTempCard firstData is : ${firstData}`);
@@ -29,11 +29,9 @@ const FirstTempCard = ({ firstData, firstOnSelectCity }) => {
         }
       })
       .then((data) => {
-        setWeatherIcon(data); // Assuming `data` contains the weather icon URL
-        console.log(
-          "FirstTempCard setWeatherIcon is:",
-          JSON.stringify(setWeatherIcon)
-        );
+        // console.log(data);
+        setFirstWeatherFetch(data); // Assuming `data` contains the weather icon URL
+        console.log("FirstTempCard firstWeatherFetch is:", firstWeatherFetch);
       })
       .catch((error) => {
         console.log(`Error fetching data: ${error}`);
@@ -42,37 +40,42 @@ const FirstTempCard = ({ firstData, firstOnSelectCity }) => {
 
   return (
     <View style={styles.container}>
-      {firstData && firstData[0] && (
+      {firstWeatherFetch && (
         <View>
-          <Text style={styles.title}>Montreal</Text>
-          <Image
-            source={{ uri: weatherIcon }}
+          <Text style={styles.title}>{firstOnSelectCity}</Text>
+          {/* <Image
+            source={{ uri: firstWeatherFetch }}
             style={styles.icon}
-            alt={"WeatherIcon"}
-          />
-          <Text style={styles.description}>{firstData[0].WeatherText}</Text>
+            alt={"firstWeatherFetch"}
+          /> */}
+          <Text style={styles.description}>
+            {firstWeatherFetch.WeatherText}
+          </Text>
           <View style={styles.InfoContainer}></View>
           <Text style={styles.description}>{`Current Temperature: ${Math.ceil(
-            firstData[0].Temperature
+            firstWeatherFetch[0].Temperature.Metric.Value
           )}°C`}</Text>
-          {/* <Text style={styles.description}>{`Max Temperature: ${Math.ceil(
-            firstData[0].TemperatureSummary.Past24HourRange.Maximum.Metric.Value
+          <Text style={styles.description}>{`Max Temperature: ${Math.ceil(
+            firstWeatherFetch[0].TemperatureSummary.Past24HourRange.Maximum
+              .Metric.Value
           )}°C`}</Text>
           <Text style={styles.description}>{`Min Temperature: ${Math.ceil(
-            firstData[0].TemperatureSummary.Past24HourRange.Minimum.Metric.Value
+            firstWeatherFetch[0].TemperatureSummary.Past24HourRange.Minimum
+              .Metric.Value
           )}°C`}</Text>
           <Text style={styles.description}>{`Real Feel: ${Math.ceil(
-            firstData[0].RealFeelTemperature.Metric.Value
-          )}°C ${firstData[0].RealFeelTemperature.Metric.Phrase}`}</Text>
+            firstWeatherFetch[0].RealFeelTemperature.Metric.Value
+          )}°C ${
+            firstWeatherFetch[0].RealFeelTemperature.Metric.Phrase
+          }`}</Text>
           <Text style={styles.description}>{`Humidity: ${Math.ceil(
-            firstData[0].RelativeHumidity
-          )}%`}</Text> */}
+            firstWeatherFetch[0].RelativeHumidity
+          )}%`}</Text>
         </View>
       )}
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     // styles
