d0ea332dc68d913306ddb9ef704293bf3dd79411
Justin <justinsinotte@gmail.com>
2023-06-20 16:33:11 -0400

    Doing small coding because of work :(

--- client/components/Comparison.js
+++ client/components/Comparison.js
@@ -20,10 +20,100 @@ const Comparison = ({
   }, [firstWeatherFetch, secondWeatherFetch]);
 
   return (
-    <View>
-      <Text>Test</Text>
+    <View style={styles.container1}>
+      {firstWeatherFetch && (
+        <View>
+          <Text style={styles.title}>{firstOnSelectCity}</Text>
+          {/* <Image
+          source={{ uri: firstWeatherFetch }}
+          style={styles.icon}
+          alt={"firstWeatherFetch"}
+        /> */}
+          <Text style={styles.description}>
+            {firstWeatherFetch.WeatherText}
+          </Text>
+          <View style={styles.InfoContainer}></View>
+          <Text style={styles.description}>{`Current Temperature: ${Math.ceil(
+            firstWeatherFetch[0].Temperature.Metric.Value
+          )}°C`}</Text>
+          <Text style={styles.description}>{`Max Temperature: ${Math.ceil(
+            firstWeatherFetch[0].TemperatureSummary.Past24HourRange.Maximum
+              .Metric.Value
+          )}°C`}</Text>
+          <Text style={styles.description}>{`Min Temperature: ${Math.ceil(
+            firstWeatherFetch[0].TemperatureSummary.Past24HourRange.Minimum
+              .Metric.Value
+          )}°C`}</Text>
+          <Text style={styles.description}>{`Real Feel: ${Math.ceil(
+            firstWeatherFetch[0].RealFeelTemperature.Metric.Value
+          )}°C ${
+            firstWeatherFetch[0].RealFeelTemperature.Metric.Phrase
+          }`}</Text>
+          <Text style={styles.description}>{`Humidity: ${Math.ceil(
+            firstWeatherFetch[0].RelativeHumidity
+          )}%`}</Text>
+        </View>
+      )}
+      <View style={styles.container2}>
+        {secondWeatherFetch && (
+          <View>
+            <Text style={styles.title}>{secondOnSelectCity}</Text>
+            {/* <Image
+            source={{ uri: secondWeatherFetch }}
+            style={styles.icon}
+            alt={"secondWeatherFetch"}
+          /> */}
+            <Text style={styles.description}>
+              {secondWeatherFetch.WeatherText}
+            </Text>
+            <View style={styles.InfoContainer}></View>
+            <Text style={styles.description}>{`Current Temperature: ${Math.ceil(
+              secondWeatherFetch[0].Temperature.Metric.Value
+            )}°C`}</Text>
+            <Text style={styles.description}>{`Max Temperature: ${Math.ceil(
+              secondWeatherFetch[0].TemperatureSummary.Past24HourRange.Maximum
+                .Metric.Value
+            )}°C`}</Text>
+            <Text style={styles.description}>{`Min Temperature: ${Math.ceil(
+              secondWeatherFetch[0].TemperatureSummary.Past24HourRange.Minimum
+                .Metric.Value
+            )}°C`}</Text>
+            <Text style={styles.description}>{`Real Feel: ${Math.ceil(
+              secondWeatherFetch[0].RealFeelTemperature.Metric.Value
+            )}°C ${
+              secondWeatherFetch[0].RealFeelTemperature.Metric.Phrase
+            }`}</Text>
+            <Text style={styles.description}>{`Humidity: ${Math.ceil(
+              secondWeatherFetch[0].RelativeHumidity
+            )}%`}</Text>
+          </View>
+        )}
+      </View>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container1: {
+    // styles
+    borderWidth: 1,
+    borderColor: "brown",
+  },
+  container2: {
+    borderWidth: 1,
+    borderColor: "pink",
+  },
+  title: {
+    // styles
+  },
+  icon: {
+    // styles
+  },
+  description: {
+    // styles
+  },
+  InfoContainer: {
+    // styles
+  },
+});
 
 export default Comparison;
